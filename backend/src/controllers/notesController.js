7cbd44cb43929e02b479102928fbfbd846bcf380
Mouloud78 <mouloudouldali78@gmail.com>
2026-08-11 17:01:04 -0400

    updateNote added

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -22,16 +22,33 @@ export async function createNote(req, res) {
     res
       .status(201)
       .json({ message: "Note created successfully!", note: newNote });
-    res.status(201).json(newNote);
   } catch (error) {
     console.log("Error in createNote controller ", error);
     res.status(500).json({ message: "Error creating note", error });
   }
 }
 
-export const updateNote = (req, res) => {
-  res.status(200).json({ message: "Note updated successfully!" });
-};
+export async function updateNote(req, res) {
+  try {
+    const { title, content } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      { title, content },
+      { new: true },
+    );
+
+    if (!updatedNote) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Note updated successfully!", note: updatedNote });
+  } catch (error) {
+    console.log("Error in updateNote controller ", error);
+    res.status(500).json({ message: "Error updating note", error });
+  }
+}
 
 export const deleteNote = (req, res) => {
   res.status(200).sjon({ message: "Note deleted successfully!" });
