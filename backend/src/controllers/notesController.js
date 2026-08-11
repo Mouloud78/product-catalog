de7ef1a941f0519b4b71b93d343be089d4053b14
Mouloud78 <mouloudouldali78@gmail.com>
2026-08-11 16:33:14 -0400

    getAllNotes method added

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,6 +1,14 @@
-export const getAllNotes = (req, res) => {
-  res.status(200).send("You just fetched the notes");
-};
+import Note from "../models/Note.js";
+
+export async function getAllNotes(req, res) {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (error) {
+    console.log("Error in getAllNotes controller ", error);
+    res.status(500).json({ message: "Error retrieving notes", error });
+  }
+}
 
 export const createNote = (req, res) => {
   res.status(201).json({ message: "Note created successfully!" });
